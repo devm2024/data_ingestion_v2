49ca2248d1bf6d245a3cc6bfcff8a1dd08b48a75
Devesh Maheshwari <dev.elect.iitd@gmail.com>
2018-02-26 18:32:50 -0800

    changed threading

---
+++ data_server.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 24 19:23:32 2018
+
+@author: deveshmaheshwari
+"""
+
+import logging
+import glob
+import time
+import os
+from threading import Thread
+from flask import Flask, request
+import json
+from logging.handlers import TimedRotatingFileHandler
+app = Flask(__name__) #create the Flask app
+from my_handler import MyTimedRotatingFileHandler
+
+
+        
+def create_timed_rotating_log(path):
+    """
+    Returns the Timed Rotating file handler
+    """
+    logger = logging.getLogger("Rotating Log")
+    logger.setLevel(logging.INFO)
+ 
+    handler = MyTimedRotatingFileHandler(path,
+                                       when="s",
+                                       interval=10,
+                                       backupCount=0)
+    
+    logger.addHandler(handler)
+    return logger
+
+
+log_file = "Raw.txt"
+logger= create_timed_rotating_log(log_file)
+
+        
+@app.route('/submit',methods = ['POST'])
+def process():
+    data = request.data
+    logger.info(data)
+    print(data)
+    return data
+
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+
+
+@app.route('/shutdown', methods=['GET'])
+def shutdown():
+    shutdown_server()
+    exit()
+    #background_thread.deamon()
+    return 'Server shutting down...'
+
+app.run(host='0.0.0.0', port=5000, debug=True)
+    
---
+++ my_handler.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 26 16:25:38 2018
+
+@author: deveshmaheshwari
+"""
+import glob
+from logging.handlers import TimedRotatingFileHandler
+from threading import Thread
+
+
+def process_file(file_name):
+    # Get file list
+    file_list =glob.glob(file_name+'*')
+    file_list = [x for x in file_list if x!= file_name]
+    file_list  = sorted(file_list, reverse=True)
+    if file_list>0:
+        file_to_read = file_list[0]
+        print('ReadingFile', file_to_read)
+        with open(file_to_read, 'r') as f:
+            json_list = f.readlines()
+            for i in json_list:
+                print i
+    
+    
+    
+        # call json_parser
+        
+class MyTimedRotatingFileHandler(TimedRotatingFileHandler):
+
+    def doRollover(self):
+        print('rollingover')
+        super(MyTimedRotatingFileHandler, self).doRollover()
+        process_thread =Thread(target=process_file, args={self.baseFilename})
+        process_thread.start()
+        process_thread.stop =True
+
